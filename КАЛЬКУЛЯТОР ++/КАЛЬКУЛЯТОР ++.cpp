92a7fea861939069d21227b11ec0528b38b0b05d
Rina00x <marinochkakek@gmail.com>
2023-12-20 14:48:45 +0300

    Добавьте файлы проекта.

---
+++ КАЛЬКУЛЯТОР ++/КАЛЬКУЛЯТОР ++.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    float num1, num2;
+    char operation;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+
+    std::cout << "Выберите операцию (+, -, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "Результат: ";
+
+    switch (operation) {
+    case '+':
+        std::cout << num1 + num2;
+        break;
+    case '-':
+        std::cout << num1 - num2;
+        break;
+    case '*':
+        std::cout << num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0)
+            std::cout << num1 / num2;
+        else
+            std::cout << "Ошибка: деление на ноль!";
+        break;
+    default:
+        std::cout << "Ошибка: некорректная операция!";
+        break;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
